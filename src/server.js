fdacbc8671aed6830bbbbcf3c0ec4680a71c7f4c
Rafsanjani N.I <45141323+maplezs@users.noreply.github.com>
2023-03-23 14:15:46 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const Hapi = require('@hapi/hapi')
 const routes = require('./routes')
 const init = async () => {
   const server = Hapi.server({
-    port: 8999,
+    port: 5000,
     host: 'localhost',
     routes: {
       cors: {
